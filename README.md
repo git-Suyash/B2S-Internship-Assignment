2b49364cf549c2d50a0b85e5366b21796e31686a
Suyash Srivastava <svtsuyash@gmail.com>
2024-06-07 13:33:56 +0530

    v1.0 Landing Page

---
+++ README.md
@@ -0,0 +1,13 @@
+# Internship Assignment
+
+## Landing Page
+
+This is a react frontend code for the given asssignment to replicate the figma file for a landing page.
+The app utilises vite with React for better optimization and developer efficiency.
+
+The src folder contains the main code for the app, where pages folder contains the landing Page which is then imported to the app.jsx and is rendered in the root div as defined in the main.jsx file.
+
+App.css is the main css file for the page.
+
+### Author 
+*SUYASH SRIVASTAVA*
---
+++ src/pages/landingPage.jsx
@@ -0,0 +1,39 @@
+function LandingPage() {
+    return (
+        <div className="preview">
+            <img className="desktop-3-2" alt="" src="Desktop - 3 2.png" />
+
+            <div className="app-landing-page-container">
+                <p className="app-landing">App Landing</p>
+                <p className="app-landing">Page Free </p>
+            </div>
+            <div className="super-awesome">Super Awesome</div>
+            <div className="source-files">Source Files</div>
+            <div className="div">
+            </div>
+            <div className="div1">
+            </div>
+            <div className="rectangle-parent">
+                <div className="group-child">
+                </div>
+                <div className="free-template">Free Template</div>
+            </div>
+            <div className="div2">
+            </div>
+            <div className="rectangle-group">
+                <div className="group-item">
+                </div>
+                <div className="figma-wrapper">
+                    <img className="figma-icon" alt="" src="figma.svg" />
+
+                </div>
+            </div>
+            <img className="preview-child" alt="" src="Group 1.svg" />
+
+            <img className="desktop-3-1" alt="" src="Desktop - 3 1.png" />
+
+        </div>
+    )
+}
+
+export default LandingPage;
